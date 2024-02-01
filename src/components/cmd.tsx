38f4cfa19f5f23ecf44560b77fcf38a92100591b
valentinKrajzelman <valentinkrajzelman@hotmail.com>
2024-02-01 16:45:33 -0300

    cosas

---
+++ src/components/cmd.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cmd = () => {
+  return (
+    <div>Cmd</div>
+  )
+}
+
+export default Cmd
---
+++ src/components/musica.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Musica = () => {
+  return (
+    <div>Musica</div>
+  )
+}
+
+export default Musica
